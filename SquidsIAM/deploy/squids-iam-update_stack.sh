add1f5bd64a6269faf3601b4ad99506634a875ff
Ethan Post <maker2413@gmail.com>
2019-04-28 08:22:27 +0000

    fixed typo in update script

--- SquidsIAM/deploy/squids-iam-update_stack.sh
+++ SquidsIAM/deploy/squids-iam-update_stack.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -x
-WorkDir=$(git rev-parse --show-toplevel)/SquidsIAM
+WorkDir="$(git rev-parse --show-toplevel)/SquidsIAM"
+
+echo $WorkDir
 
 aws cloudformation update-stack \
     --capabilities CAPABILITY_NAMED_IAM \
     --stack-name squids-iam-self-delete \
     --template-body file://$WorkDir/squids-iam.json \
-    --parameters file://$WordDir/config/squids-iam-param.json \
+    --parameters file://$WorkDir/config/squids-iam-param.json \
     --tags Key=Project,Value=Squids
