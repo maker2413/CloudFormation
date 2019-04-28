32265b6ebf56c494d62266c1f821014351e3ff08
Ethan Post <maker2413@gmail.com>
2019-04-28 08:15:04 +0000

    updated scripts in deploy to use the new iam parameter file

--- SquidsIAM/deploy/squids-iam-create_stack.sh
+++ SquidsIAM/deploy/squids-iam-create_stack.sh
@@ -5,5 +5,6 @@ aws cloudformation create-stack \
     --capabilities CAPABILITY_NAMED_IAM \
     --stack-name squids-iam-self-delete \
     --template-body file://$WorkDir/squids-iam.json \
+    --parameters file://$WorkDir/config/squids-iam-param.json \
     --timeout-in-minutes 30 \
     --tags Key=Project,Value=Squids
--- SquidsIAM/deploy/squids-iam-update_stack.sh
+++ SquidsIAM/deploy/squids-iam-update_stack.sh
@@ -5,4 +5,5 @@ aws cloudformation update-stack \
     --capabilities CAPABILITY_NAMED_IAM \
     --stack-name squids-iam-self-delete \
     --template-body file://$WorkDir/squids-iam.json \
+    --parameters file://$WordDir/config/squids-iam-param.json \
     --tags Key=Project,Value=Squids
