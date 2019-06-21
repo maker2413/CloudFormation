71574644c3f2af8fc7c1988105014140d3f93ec2
Ethan Post <maker2413@gmail.com>
2019-06-21 09:42:36 +0000

    Made S3 cloudformation deployable for multiple static sites

--- SquidsS3/deploy/s3-update_stack.sh
+++ SquidsS3/deploy/s3-update_stack.sh
@@ -3,7 +3,7 @@ WorkDir=$(git rev-parse --show-toplevel)/SquidsS3
 
 aws cloudformation update-stack \
     --capabilities CAPABILITY_NAMED_IAM \
-    --stack-name squids-s3-self-delete \
-    --template-body file://$WorkDir/squids-s3.json \
-    --parameters file://$WorkDir/config/squids-s3-param.json \
+    --stack-name $1-s3-self-delete \
+    --template-body file://$WorkDir/$1-s3.json \
+    --parameters file://$WorkDir/config/s3-param.json \
     --tags Key=Project,Value=Squids
