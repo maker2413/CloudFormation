4932f71def060a13c871e2384cd98867d32ea472
maker2413 <maker2413@gmail.com>
2021-05-13 19:30:47 -0700

    dustin's website

---
+++ DustinS3/update-terraform-stack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+WorkDir=$(git rev-parse --show-toplevel)/TerraformS3
+
+echo 'Updating S3 Stack'
+aws cloudformation update-stack \
+    --stack-name terraform-service-s3 \
+    --template-body file://$WorkDir/s3.yml \
+    --tags Key=Project,Value=Squids
